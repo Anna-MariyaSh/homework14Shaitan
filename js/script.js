0ba2bae317edefa8c8ce5108488fe6eaa0ff7e42
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-10-30 18:03:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
-let string = prompt("put your string");
-let number = +prompt("put your number");
-let symbal = prompt("put your symbol");
-let bullin = prompt("true or false");
-function padString(str, num, smb, bull) {
+function padString(str, num, smb, bull = true) {
 
- if (isNaN(num)) {
-   return alert("error in your length");
+ if (typeof num !== "number" && isNaN(num)) return alert("error in your length");
 
- } else if (smb === null) {
-   return alert("error in your symbol");
+  if (typeof smb !== "string" && smb.length !== 1) return alert("error in your symbol");
 
- } else if (bull !== "true" && bull !== "false") {
-   return alert("error in plase your symbal");
+  if (typeof bull !== "boolean") return alert("error in plase your symbal");
 
- } else if (str === null) {
-   return alert("error in your string");
+  if (typeof str !== "string") return alert("error in your string");
 
- } else if(bull === "true" && str.length <= num) {
-   return console.log(str + smb.repeat(num - str.length));
+  if(str.length >= num) return str.substring(0, num);
 
- } else if (bull === "false" && str.length <= num) {
-    return console.log(smb.repeat(num - str.length) +str);
-
- } else if(str.length >= num) {
-   return console.log(str.substr(0,num));
-
- }
-
+  for(let i = str.length; i <= num; i++) {
+    if(bull) {
+      str += smb;
+    } else {
+      str = smb + str;
+    }
+  }
+ return str;
 }
-padString(string, number, symbal, bullin);
+console.log(padString("hgh", 9, "c", false));
